8f6d206c116a044a1664807d259fd047971745e8
derek chen <derek.g.chen@gmail.com>
2018-03-23 16:26:59 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -17,9 +17,6 @@ def hopper
      }
 
 		 programmer_hash[:grace_hopper]
-
-
-
 end
 
 def alan_kay_is_known_for
@@ -40,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
