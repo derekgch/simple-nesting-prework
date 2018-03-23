8501a062961836e226534d5d7b1d5a6dfee7ccd1
derek chen <derek.g.chen@gmail.com>
2018-03-23 16:31:46 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages]
+		 temp = programmer_hash[:dennis_ritchie][:languages]
+		 temp.join
 end
 
 def adding_matz
